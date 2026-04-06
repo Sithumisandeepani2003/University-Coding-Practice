2686a1e5bff7e6f9f6000faab288bd53f8e448f8
Sithumi Sandeepani Ranathunga <sandeepaniranathunga2003@gmail.com>
2026-04-06 16:47:01 +0530

    Add my first code file

---
+++ q1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+
+int day=1;
+switch(day){
+case 1:
+    printf("monday");
+    break;
+case 2:
+    printf("tuesday");
+    break;
+case 3:
+    printf("wednasday");
+    break;
+default:
+    printf("invali day");
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
